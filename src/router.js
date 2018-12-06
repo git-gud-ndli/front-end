3d071b605bcb2885b41d4a981cb381d99efe0dc5
Paul <rahveiz@gmail.com>
2018-12-06 23:04:52 +0100

    add todo

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Dashboard from "./views/Dashboard.vue";
+import Todo from "./views/Todo.vue";
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: "/dashboard",
       name: "dashboard",
       component: Dashboard
+    },
+    {
+      path: "/todo",
+      name: "todo",
+      component: Todo
     },
     {
       path: "/about",
---
+++ src/views/Todo.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="todo">
+    <h1 class="display-4 text-xs-center">To-do list</h1>
+    <v-container grid-list-xs>
+      <v-layout row justify-center>
+        <v-flex xs6>
+          <v-list v-for="(list, index) in todo_lists.lists" :key="index">
+            <v-list-tile
+              v-for="(item, key) in list.items"
+              :key="key"
+              avatar
+              @click="revertCheck(key)"
+            >
+              <v-list-tile-action>
+                <v-icon v-if="item.checked" color="pink">star</v-icon>
+              </v-list-tile-action>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-text="item.name"></v-list-tile-title>
+              </v-list-tile-content>
+
+              <v-list-tile-avatar></v-list-tile-avatar>
+            </v-list-tile>
+          </v-list>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </section>
+</template>
+
+<script lang="js">
+import GETLIST from "@/graphql/GetTodo.gql";
+  export default  {
+    name: 'todo',
+    props: [],
+    mounted() {
+
+    },
+    data() {
+      return {
+      }
+    },
+    apollo: {
+      todo_lists: GETLIST
+    },
+    methods: {
+      revertCheck(key) {
+        /*
+        this.$apollo.mutate({
+          mutation: CHECKITEM,
+          variables: {
+            id: this.todo[key].id,
+            value: !this.todo[key].checked
+          }
+        });
+        this.$apollo.queries.GETLIST.refetch();
+        */
+      }
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="less">
+.todo {
+}
+</style>
