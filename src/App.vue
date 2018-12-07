bbd628dec557b4fc47b30b7c27199431d08a3135
Timothée Oliger <oliger.timothee@gmail.com>
2018-12-07 04:23:41 +0100

    la liberté

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span id="tn">GIT-GUD(1)</span>
-        <span class="font-weight-light" style="margin-left: 20px;">LA NUIT DE L'INFO</span>
+        <span class="font-weight-light" style="margin-left: 20px;"
+          >LA NUIT DE L'INFO</span
+        >
         <select v-on:change="changeLang" v-model="language" class="sel">
           <option value="en">🇬🇧</option>
           <option value="fr">🇫🇷</option>
@@ -12,19 +14,15 @@
       <v-spacer></v-spacer>
       <span v-if="isLogged">
         <v-btn flat @click="dashboard">
-          {{
-          $vuetify.t("$vuetify.home.dashboard")
-          }}
+          {{ $vuetify.t("$vuetify.home.dashboard") }}
         </v-btn>
         <v-btn flat @click="logout">Sign Out</v-btn>
       </span>
-      <span v-else>
-        <NavLinks/>
-      </span>
+      <span v-else> <NavLinks /> </span>
     </v-toolbar>
 
     <v-content>
-      <back-button v-if="currentPath"/>
+      <back-button v-if="currentPath" />
       <router-view></router-view>
     </v-content>
   </v-app>
@@ -50,22 +48,6 @@ export default {
       },
       language: "en"
     };
-  },
-  created() {
-    navigator.geolocation.getCurrentPosition(location => {
-      this.gps.long = location.coords.longitude;
-      this.gps.lat = location.coords.accuracy;
-      // push au serv ses coordonnées
-      Push.create("CA MARCHE", {
-        body: "ça compile donc ça marche",
-        icon: "/img/icons/favicon-32x32.png",
-        timeout: 4000,
-        onClick: function() {
-          window.focus();
-          this.close();
-        }
-      });
-    });
   },
   computed: {
     ...mapGetters({
